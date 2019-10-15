ad4f4805eee094804f8a3a52c8234de22e43f605
chenpenghui <chenpenghui@hisense.com>
2019-10-15 22:45:49 +0800

    update configuration

--- src/main/java/com/cph/MyApp2.java
+++ src/main/java/com/cph/MyApp2.java
@@ -1,7 +1,6 @@
 package com.cph;
 
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.util.ContextInitializer;
 import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
