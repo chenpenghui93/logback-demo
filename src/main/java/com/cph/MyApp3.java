86f84fd1499343e49858a432c8a8d62c71f7b8cf
chenpenghui <chenpenghui@hisense.com>
2019-10-16 12:28:21 +0800

    update configuration

--- src/main/java/com/cph/MyApp3.java
+++ src/main/java/com/cph/MyApp3.java
@@ -15,20 +15,22 @@ public class MyApp3 {
     final static Logger logger = LoggerFactory.getLogger(MyApp3.class);
 
     public static void main(String[] args) {
-        LoggerContext context = (LoggerContext)LoggerFactory.getILoggerFactory();
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
 
         try {
             JoranConfigurator configurator = new JoranConfigurator();
             configurator.setContext(context);
+            // Call context.reset() to clear any previous configuration, e.g. default
+            // configuration. For multi-step configuration, omit calling context.reset().
             context.reset();
             configurator.doConfigure(args[0]);
-        } catch (JoranException e) {
-            e.printStackTrace();
+        } catch (JoranException je) {
+            // StatusPrinter will handle this
         }
-
         StatusPrinter.printInCaseOfErrorsOrWarnings(context);
 
         logger.info("Entering application.");
+
         Foo foo = new Foo();
         foo.doIt();
         logger.info("Exiting application.");
